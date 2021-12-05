6f3c0eb17d20ee6e6ef6716eadb2834d91c57038
Akinori Noguchi <gbl.6sep@gmail.com>
2021-12-05 16:07:46 +0900

    updated to 7

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,7 +43,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Top ver.6
+          Top ver.7
         </h1>
         <div className={styles.linkWrap}>
           <Link href='/main'>Mainへ</Link>
