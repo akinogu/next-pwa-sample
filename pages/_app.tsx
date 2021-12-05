fa01acf9748662a15ddf917cf0411fe652e2c9f2
Akinori Noguchi <gbl.6sep@gmail.com>
2021-12-06 02:32:54 +0900

    fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,18 +24,18 @@ function MyApp({ Component, pageProps }: AppProps) {
         // `event.wasWaitingBeforeRegister` will be false if this is the first time the updated service worker is waiting.
         // When `event.wasWaitingBeforeRegister` is true, a previously updated service worker is still waiting.
         // You may want to customize the UI prompt accordingly.
-        // if (confirm('新しいバージョンが利用できます。リロードしますか？')) {
-        //   wb.addEventListener('controlling', () => {
-        //     window.location.reload()
-        //   })
+        if (confirm('新しいバージョンが利用できます。リロードしますか？')) {
+          wb.addEventListener('controlling', () => {
+            window.location.reload()
+          })
 
-        //   // Send a message to the waiting service worker, instructing it to activate.
-        //   wb.messageSkipWaiting()
-        // } else {
-        //   console.log(
-        //     'User rejected to reload the web app, keep using old version. New version will be automatically load when user open the app next time.'
-        //   )
-        // }
+          // Send a message to the waiting service worker, instructing it to activate.
+          wb.messageSkipWaiting()
+        } else {
+          console.log(
+            'User rejected to reload the web app, keep using old version. New version will be automatically load when user open the app next time.'
+          )
+        }
       }
       wb.addEventListener('installed', (evnet: any) => {
         console.log('----- installed', event)
