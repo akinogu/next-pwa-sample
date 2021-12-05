3d424c80bde5c5a2d951bfde4dcf9dbd569bb50d
Akinori Noguchi <gbl.6sep@gmail.com>
2021-12-06 00:10:59 +0900

    updated to 11

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+// const runtimeCacåhing = require('next-pwa/cache')
 
 module.exports = withPWA({
   pwa: {
     dest: 'public',
-    runtimeCaching,
+    // runtimeCaching,
     buildExcludes: [/middleware-manifest.json$/],
     register: false,
-    // skipWaiting: false
+    skipWaiting: false
   },
   reactStrictMode: true,
 })
--- pages/index.tsx
+++ pages/index.tsx
@@ -48,7 +48,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Top ver.10
+          Top ver.11
         </h1>
         <div className={styles.linkWrap}>
           <Link href='/main'>Mainへ</Link>
