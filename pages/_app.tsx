0ff2ad485bf7255a22e5132b021db2a103bb8b26
Akinori Noguchi <gbl.6sep@gmail.com>
2021-12-06 02:25:28 +0900

    add debug code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  console.log(router)
+  useEffect(() => {
+    console.log('--- path changed')
+  }, [router.pathname])
   useEffect(() => {
     if (typeof window !== 'undefined' && 'serviceWorker' in navigator && window.workbox !== undefined) {
       const wb = window.workbox
